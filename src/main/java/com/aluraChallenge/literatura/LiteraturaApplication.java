3bc29edaa2249275bf3a3944d78f3504fa78739c
Carlos Jonatan Bordón <68975079+YonaBordon@users.noreply.github.com>
2024-11-20 00:07:14 -0300

    Items 1 y 2

---
+++ src/main/java/com/aluraChallenge/literatura/LiteraturaApplication.java
@@ -0,0 +1,23 @@
+package com.aluraChallenge.literatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaApplication {
+
+	//TODO: 3) Construyendo una solicitud de API
+	//TODO: 4) Analizando la respuesta en formato JSON
+	//TODO:	5) Convertiendo los datos
+	//TODO: 6) Interactuando con el usuario
+	//TODO: 7) Consultar libros
+	//TODO: 8) Consultar autores
+	//TODO: 9) Persistencia de datos
+	//TODO: 10) Listando libros por idiomas
+	//TODO: 11) Listando autores vivos en determinado año
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturaApplication.class, args);
+	}
+
+}
